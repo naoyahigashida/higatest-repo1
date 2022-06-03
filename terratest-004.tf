41cd8390baae989e4a8229e4b735c4ae4c84f48e
naoyahigashida <99231041+naoyahigashida@users.noreply.github.com>
2022-06-03 14:56:58 +0900

    Add files via upload
    deploy

---
+++ terratest-004.tf
@@ -0,0 +1,9 @@
+module "terratest-004" {
+  source = "./modules"
+  hostname = "terratest-004"
+  resource_group_name = "terraform-test"
+  subnet_name = "vnet2-subnet1"
+  IPAddress = "172.168.160.28"
+  vmsize = "Standard_B2s"
+  managed_disk_type = "StandardSSD_LRS"
+}
