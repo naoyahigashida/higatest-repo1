7e9814a1625481b1189beba887215ed682759d53
naoyahigashida <99231041+naoyahigashida@users.noreply.github.com>
2022-06-03 15:02:26 +0900

    Update terratest-003.tf

--- terratest-003.tf
+++ terratest-003.tf
@@ -3,7 +3,7 @@ module "terratest-003" {
   hostname = "terratest-003"
   resource_group_name = "terraform-test2"
   subnet_name = "vnet2-subnet1"
-  IPAddress = "172.168.160.27"
+  IPAddress = "172.168.160.28"
   vmsize = "Standard_B2ms"
   managed_disk_type = "StandardSSD_LRS"
 }
