f23467331995193dfd9717952eb7aeaf705661ef
naoyahigashida <99231041+naoyahigashida@users.noreply.github.com>
2022-06-03 15:20:24 +0900

    Delete terratest-003.tf
    server remove

--- terratest-003.tf
+++
@@ -1,9 +0,0 @@
-module "terratest-003" {
-  source = "./modules"
-  hostname = "terratest-003"
-  resource_group_name = "terraform-test2"
-  subnet_name = "vnet2-subnet1"
-  IPAddress = "172.168.160.28"
-  vmsize = "Standard_B2ms"
-  managed_disk_type = "StandardSSD_LRS"
-}
