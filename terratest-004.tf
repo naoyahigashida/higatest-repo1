8ac77dc4bbc752fb9641a2372d9a76f3ad9c7a18
naoyahigashida <99231041+naoyahigashida@users.noreply.github.com>
2022-06-09 10:58:19 +0900

    Delete terratest-004.tf

--- terratest-004.tf
+++
@@ -1,9 +0,0 @@
-module "terratest-004" {
-  source = "./modules"
-  hostname = "terratest-004"
-  resource_group_name = "terraform-test"
-  subnet_name = "vnet2-subnet1"
-  IPAddress = "172.168.160.28"
-  vmsize = "Standard_B2ms"
-  managed_disk_type = "StandardSSD_LRS"
-}
